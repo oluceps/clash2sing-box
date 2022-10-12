a65a144c2d6f7c32377c2fec9e459ca52155c4e5
oluceps <i@oluceps.uk>
2022-10-12 20:33:16 +0800

    refactor: initialization

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 use serde::{Deserialize, Serialize};
-use yaml_rust::YamlLoader;
+use serde_json::to_string;
+use yaml_rust::{Yaml, YamlLoader};
 
 #[allow(dead_code)]
 enum AvalProtocals {
-    SOCKS,
-    HTTP,
-    Shadowsocks,
+    SOCKS {
+        tag: String,
+        server: String,
+        server_port: u16,
+        version: u16,
+        username: String,
+        password: String,
+        network: String,
+        udp_over_tcp: bool,
+    },
+    HTTP {
+        tag: String,
+        server: String,
+        server_port: u16,
+        username: String,
+        password: String,
+        tls: TLS,
+    },
+    Shadowsocks {
+        tag: String,
+        server: String,
+        server_port: u16,
+        method: String,
+        password: String,
+        plugin: String,
+        plugin_opts: String,
+        network: String,
+        udp_over_tcp: bool,
+        multiplex: Option<Multiplex>,
+    },
     VMess,
     Trojan,
     Hysteria,
@@ -31,106 +59,21 @@ struct Multiplex {
     max_streams: u16,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct SOCKS {
-    tag: String,
-    server: String,
-    server_port: u16,
-    version: u16,
-    username: String,
-    password: String,
-    network: String,
-    udp_over_tcp: bool,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct HTTP {
-    tag: String,
-    server: String,
-    server_port: u16,
-    username: String,
-    password: String,
-    tls: TLS,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Shadowsocks {
-    tag: String,
-    server: String,
-    server_port: u16,
-    method: String,
-    password: String,
-    plugin: String,
-    plugin_opts: String,
-    network: String,
-    udp_over_tcp: bool,
-    multiplex: Option<Multiplex>,
-}
 
 #[allow(unused)]
 trait CouldBeConvert {
-    fn new() -> Self;
-
     fn convert(&self, yaml_data: yaml_rust::Yaml) -> () {}
 }
 
-#[allow(unused)]
-impl CouldBeConvert for SOCKS {
-    fn new() -> SOCKS {
-        SOCKS {
-            tag: "".to_string(),
-            server: "".to_string(),
-            server_port: 0,
-            version: 0,
-            username: "".to_string(),
-            password: "".to_string(),
-            network: "".to_string(),
-            udp_over_tcp: false,
+impl CouldBeConvert for AvalProtocals {
+    fn convert(&self, yaml_data: Yaml) -> () {
+        match yaml_data["type"].clone().into_string().unwrap().as_str() {
+            "ss" => println!("tested!"),
+            _ => println!("fail"),
         }
     }
-    fn convert(&self, yaml_data: yaml_rust::Yaml) -> () {}
 }
 
-#[allow(unused)]
-impl CouldBeConvert for HTTP {
-    fn new() -> HTTP {
-        HTTP {
-            tag: "".to_string(),
-            server: "".to_string(),
-            server_port: 0,
-            username: "".to_string(),
-            password: "".to_string(),
-            tls: TLS {},
-        }
-    }
-    fn convert(&self, yaml_data: yaml_rust::Yaml) -> () {}
-}
-
-#[allow(unused)]
-impl CouldBeConvert for Shadowsocks {
-    fn new() -> Shadowsocks {
-        Shadowsocks {
-            tag: "".to_string(),
-            server_port: 0,
-            server: "".to_string(),
-            method: "".to_string(),
-            password: "".to_string(),
-            plugin: "".to_string(),
-            plugin_opts: "".to_string(),
-            network: "".to_string(),
-            udp_over_tcp: false,
-            multiplex: Some(
-                Multiplex {
-                    enable: false,
-                    protocal: "smux".to_string(),
-                    max_connections: 0,
-                    min_streams: 0,
-                    max_streams: 0,
-                }),
-        }
-    }
-    fn convert(&self, yaml_data: yaml_rust::Yaml) -> () {}
-}
 
 #[allow(unused)]
 fn main() {
@@ -145,12 +88,59 @@ proxies:
     // single node: yaml_test["proxies"][n]
     let node_list: Vec<i8> = vec![];
 
+
     for single_node in yaml_test["proxies"].clone() {
         println!("{:?}", single_node["server"]);
 
         let protocols = "ss".to_string();
-        let processed_node = match single_node["type"].clone().into_string() {
-            protocal => Shadowsocks::new(),
+        let processed_node = match single_node["type"].clone().into_string().unwrap().as_str() {
+            "ss" => AvalProtocals::Shadowsocks {
+                tag: "".to_string(),
+                server_port: 0,
+                server: "".to_string(),
+                method: "".to_string(),
+                password: "".to_string(),
+                plugin: "".to_string(),
+                plugin_opts: "".to_string(),
+                network: "".to_string(),
+                udp_over_tcp: false,
+                multiplex: Some(
+                    Multiplex {
+                        enable: false,
+                        protocal: "smux".to_string(),
+                        max_connections: 0,
+                        min_streams: 0,
+                        max_streams: 0,
+                    }),
+            },
+            "http" => AvalProtocals::HTTP {
+                tag: "".to_string(),
+                server: "".to_string(),
+                server_port: 0,
+                username: "".to_string(),
+                password: "".to_string(),
+                tls: TLS {},
+            },
+            "socks" => AvalProtocals::SOCKS {
+                tag: "".to_string(),
+                server: "".to_string(),
+                server_port: 0,
+                version: 0,
+                username: "".to_string(),
+                password: "".to_string(),
+                network: "".to_string(),
+                udp_over_tcp: false,
+            },
+            &_ => AvalProtocals::SOCKS {
+                tag: "".to_string(),
+                server: "".to_string(),
+                server_port: 0,
+                version: 0,
+                username: "".to_string(),
+                password: "".to_string(),
+                network: "".to_string(),
+                udp_over_tcp: false,
+            },
         }.convert(single_node);
     }
 
