8c530545d101ff022dd24b8563d40420ce89741f
oluceps <i@oluceps.uk>
2022-10-30 16:51:11 +0800

    add: nix aarch linux gnu build

--- flake.nix
+++ flake.nix
@@ -17,6 +17,27 @@
       packages.default =
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          target = "x86_64-unknown-linux-gnu";
+          toolchain = with fenix.packages.${system}; combine [
+            minimal.cargo
+            minimal.rustc
+            targets.${target}.latest.rust-std
+          ];
+        in
+
+        with (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }); buildPackage {
+          src = ./.;
+          CARGO_BUILD_TARGET = target;
+          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+            "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${target}-gcc";
+        };
+
+      packages.aarch64-unknown-linux-gnu =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
           target = "aarch64-unknown-linux-gnu";
           toolchain = with fenix.packages.${system}; combine [
             minimal.cargo
@@ -25,10 +46,10 @@
           ];
         in
 
-        (naersk.lib.${system}.override {
+        with (naersk.lib.${system}.override {
           cargo = toolchain;
           rustc = toolchain;
-        }).buildPackage {
+        }); buildPackage {
           src = ./.;
           CARGO_BUILD_TARGET = target;
           CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
