eee73342f234fa1c1bcb55645b9987b55771a08f
oluceps <i@oluceps.uk>
2023-11-03 20:40:12 +0800

    bump lock

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, fenix, flake-utils, nixpkgs }:
+  outputs = { self, fenix, flake-utils, nixpkgs, naersk }:
 
     flake-utils.lib.eachSystem
       (with flake-utils.lib.system;
@@ -18,12 +19,11 @@
           pkgs = import nixpkgs { inherit system; };
           version = pkgs.lib.substring 0 8 self.lastModifiedDate
             or self.lastModified or "19700101";
+          naersk' = pkgs.callPackage naersk { };
         in
         rec {
           packages.default =
-            (pkgs.makeRustPlatform {
-              inherit (fenix.packages.${system}.minimal) cargo rustc;
-            }).buildRustPackage
+            naersk'.buildPackage
               {
                 name = "clash2sing-box";
 
@@ -31,12 +31,12 @@
                 inherit version;
                 cargoBuildFlags = "-p ctos";
 
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                  outputHashes = {
-                    "yew-0.20.0" = "sha256-x1Z85JMpeJqz1R8D4dbMLol06ZN0bVyIuA057H6Ce38=";
-                  };
-                };
+                # cargoLock = {
+                #   lockFile = ./Cargo.lock;
+                #   outputHashes = {
+                #     "yew-0.20.0" = "sha256-x1Z85JMpeJqz1R8D4dbMLol06ZN0bVyIuA057H6Ce38=";
+                #   };
+                # };
 
 
                 mainProgram = "ctos-${system}";
